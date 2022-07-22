abf357a7fdd4029b1309de187cc959e462a75196
Hooseinsalari <hossein.salari002@gmail.com>
2022-07-22 19:43:31 +0430

    finish project

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
-const prevBtns = document.querySelectorAll('.btn-prev');
-const nextBtns = document.querySelectorAll('.btn-next');
-const progress = document.querySelector('#progress');
-const progressSteps = document.querySelectorAll('.progress-step');
-const formSteps = document.querySelectorAll('.form-step');
+const prevBtns = document.querySelectorAll(".btn-prev");
+const nextBtns = document.querySelectorAll(".btn-next");
+const progress = document.querySelector("#progress");
+const progressSteps = document.querySelectorAll(".progress-step");
+const formSteps = document.querySelectorAll(".form-step");
 
 let formStepNum = 0;
 
 nextBtns.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        formStepNum++;
-        updateFormStep();
-        updateProgressBar();
-    })
-})
+  btn.addEventListener("click", () => {
+    formStepNum++;
+    updateFormStep();
+    updateProgressBar();
+  });
+});
 
 prevBtns.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        formStepNum--;
-        updateFormStep();
-        updateProgressBar();
-    })
-})
+  btn.addEventListener("click", () => {
+    formStepNum--;
+    updateFormStep();
+    updateProgressBar();
+  });
+});
 
 const updateFormStep = () => {
-    formSteps.forEach((formStep) => {
-        formStep.classList.contains('form-step-active') && 
-        formStep.classList.remove('form-step-active')
-    })    
+  formSteps.forEach((formStep) => {
+    formStep.classList.contains("form-step-active") &&
+      formStep.classList.remove("form-step-active");
+  });
 
-    formSteps[formStepNum].classList.add('form-step-active');
-}
+  formSteps[formStepNum].classList.add("form-step-active");
+};
 
 const updateProgressBar = () => {
-    progressSteps.forEach((progressStep, idx) => {
-        if(idx < formStepNum + 1) {
-            progressStep.classList.add('progress-step-active')
-        } else {
-            progressStep.classList.remove('progress-step-active')
-        }
-    })
+  progressSteps.forEach((progressStep, idx) => {
+    if (idx < formStepNum + 1) {
+      progressStep.classList.add("progress-step-active");
+    } else {
+      progressStep.classList.remove("progress-step-active");
+    }
+  });
 
-    const progressStepActive = document.querySelectorAll('.progress-step-active')
-    progress.style.width = ((progressStepActive.length - 1) / (progressSteps.length - 1)) * 100 + '%';
-}
+  const progressStepActive = document.querySelectorAll(".progress-step-active");
+  progress.style.width =
+    ((progressStepActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
+};
